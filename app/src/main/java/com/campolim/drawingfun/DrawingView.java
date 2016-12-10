01a329a694b999617fd15b5eff77e73eea9c9761
campolim <melissa.campoli@mail.utoronto.ca>
2016-12-10 16:57:25 -0500

    First module of tutorial: Interface Creation complete

---
+++ app/src/main/java/com/campolim/drawingfun/DrawingView.java
@@ -0,0 +1,22 @@
+package com.campolim.drawingfun;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by mcampoli on 10-Dec-16.
+ */
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+
+}
